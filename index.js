3c9b3b30243e4354edab4dca42edccc04eea9c4e
sumeet <sumeetdongre123@gmail.com>
2020-06-12 16:31:24 +0530

    flash messages login and log out

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo')(session);
 const sassMiddleware =require('node-sass-middleware');
 
+const flash=require('connect-flash');
+const custommiddlware=require('./config/middleware');
 
 app.use(sassMiddleware({
     src: './assets/scss',
@@ -60,11 +62,14 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+
 app.use(passport.setAuthenticatedUser);
+
+app.use(flash());
+app.use(custommiddlware.setFlash);
 
 // use express router
 app.use('/', require('./routes'));
-
 
 app.listen(port, function(err){
     if (err){
