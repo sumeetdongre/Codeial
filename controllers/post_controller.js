dbc39fddcd0d07383c8949185d838fd816d006ee
sumeet <sumeetdongre123@gmail.com>
2020-06-23 12:06:40 +0530

    added posts in json response

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -6,12 +6,22 @@ module.exports.create=async function(req,res){
         content:req.body.content,
         user: req.user._id
       });
+
+      if(req.xhr){
+          return res.status(200).json({
+              data:{
+                  post: post
+              },
+              message:"Post created!"
+          });
+      }
       req.flash('success','Post published');
       return res.redirect('back');
+
     }catch(err){
         req.flash('error',err);
         // console.log('Error',err);
-        return;
+        return res.redirect('back');
     }
 }
 
