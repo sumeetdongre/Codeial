d41f7bc84c25dc24c179ffbd984c6f63a896275a
sumeet <sumeetdongre123@gmail.com>
2020-07-12 13:20:37 +0530

    social authentication using google o-auth

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ const usersController = require('../controllers/users_controller');
 
 router.get('/profile/:id', passport.checkAuthentication, usersController.profile);
 router.post('/update/:id',passport.checkAuthentication,usersController.update);
+
 router.get('/sign-up', usersController.signUp);
 router.get('/sign-in', usersController.signIn);
 
@@ -20,5 +21,10 @@ router.post('/create-session', passport.authenticate(
 
 
 router.get('/sign-out', usersController.destroySession);
+
+
+router.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
+router.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/users/sign-in'}), usersController.createSession);
+
 
 module.exports = router;
