06e793078b108aa15bfde5230ff7b96b045dbf51
sumeet <sumeetdongre123@gmail.com>
2020-08-03 16:35:22 +0530

    added developement and production mode

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const env=require('./config/environment');
+const logger=require('morgan');
 const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
@@ -23,24 +25,28 @@ const chatSockets=require('./config/chat_sockets').chatSockets(chatServer);
 
 chatServer.listen(5000);
 console.log("char server listening on port 5000");
+const path=require('path');
+const { ENODEV } = require('constants');
 
+if(env.name=='development'){
 app.use(sassMiddleware({
-    src: './assets/scss',
-    dest: './assets/css',
+    src: path.join(__dirname,env.asset_path,'scss'),
+    dest: path.join(__dirname,env.asset_path,'css'),
     debug: true,
     outputStyle: 'extended',
     prefix: '/css'
 }));
+}
 
 app.use(express.urlencoded());
 
 app.use(cookieParser());
 
-app.use(express.static('./assets'));
+app.use(express.static(env.asset_path));
 
 // make the uploads path available to the browser
 app.use('/uploads', express.static(__dirname + '/uploads'));
-
+app.use(logger(env.morgan.mode,env.morgan.options));
 app.use(expressLayouts);
 // extract style and scripts from sub pages into the layout
 app.set('layout extractStyles', true);
@@ -57,7 +63,7 @@ app.set('views', './views');
 app.use(session({
     name: 'codeial',
     // TODO change the secret before deployment in production mode
-    secret: 'blahsomething',
+    secret: env.session_cookie_key,
     saveUninitialized: false,
     resave: false,
     cookie: {
